2836c81142aa7b269d82d0ce866941e8364b320f
Junbro0708 <aktm708@naver.com>
2022-09-21 23:17:43 -0700

    Changed the paths and make buttons

--- src/pages/PostDashboard.js
+++ src/pages/PostDashboard.js
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 function PostDashboard(){
+  const navigate = useNavigate();
+
+  const goToEditPost = () => {
+    navigate("/editpost");
+  }
   return(
     <>
       <h1>Post Dashboard Page</h1>
+      <button onClick={goToEditPost}>Edit Post</button>
     </>
   )
 }
