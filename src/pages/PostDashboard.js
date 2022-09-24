1692353b53928174a56aa6236d0c61587bd7f894
otatakahiro <ota7281@gmail.com>
2022-09-24 13:15:29 -0700

    taka

--- src/pages/PostDashboard.js
+++ src/pages/PostDashboard.js
@@ -1,15 +1,62 @@
 import { useNavigate } from "react-router-dom";
+import {useState} from "react";
 
-function PostDashboard(){
+function Rows(props){
   const navigate = useNavigate();
-
-  const goToEditPost = () => {
+  const goToEditPost = (index) => {
     navigate("/editpost");
   }
+
+  const deletePost = (index) => {
+
+  }
+  return(
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td><button onClick={deletePost}>Delete Post</button></td>
+      <td><button onClick={goToEditPost}>Edit Post</button></td>
+    </tr>
+  )
+}
+
+function PostDashboard(){
+  const [postdata,setPostdata] = useState({
+    userId:"",
+    postUid:"",
+    postDate:"",
+    photoSrc:"",
+    tags:"",
+    addr:""
+  });
+  const [table,setTable] = useState([]);
   return(
     <>
       <h1>Post Dashboard Page</h1>
-      <button onClick={goToEditPost}>Edit Post</button>
+      <table>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>Post ID</th>
+            <th>Post data</th>
+            <th>Photo src</th>
+            <th>Tags</th>
+            <th>Addr</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table.map((post,idx)=>{
+            return <Rows key={idx} post={post} index={idx}/> 
+          })}
+          
+        </tbody>
+      </table>      
     </>
   )
 }
