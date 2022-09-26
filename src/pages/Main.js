ede42e3c4da53e1cf61c7530336536f73ccc9957
LukeShin3022 <jwshin3022@gmail.com>
2022-09-26 01:26:16 -0700

    Merge main Func by luke

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// import { useState,useEffect } from 'react';
-import { useNavigate} from 'react-router-dom';
-// import imgLoadSrv from '../services/imgLoadSrv';
-import data from '../services/post_tb';
-
-function Content(props){
-  // const user = props.user;
-  const navigate = useNavigate();
-  // const [imgData,setImg] = useState();
-=======
 import { useState,useEffect } from 'react';
 import { useNavigate} from 'react-router-dom';
 import imgLoadSrv from '../services/imgLoadSrv';
@@ -16,29 +5,10 @@ import imgLoadSrv from '../services/imgLoadSrv';
 function Content(props){
   const navigate = useNavigate();
   const [imgData,setImg] = useState([]);
->>>>>>> jun
 
   const goToPostDetail = () => {
     navigate("/postdetail");
   };
-<<<<<<< HEAD
-  //let username = {user};
-  // imgLoadSrv.load(username)
-  //   .then(response =>{
-  //     setImg(response);
-  //   })
-  //   .catch(err=>{
-  //     console.log(err);
-  //   });
-
-  // useEffect(()=>{
-  //      console.log(imgData);
-  //   },[]);
-  return(
-    <>
-      { data.map((v,idx) => (
-        <figure key={idx} className="content" onClick={goToPostDetail()}>
-=======
   imgLoadSrv.load()
     .then(response =>{
       setImg(response.data);
@@ -50,7 +20,6 @@ function Content(props){
     <>
       {imgData.map((v, idx) => (
         <figure key={idx} className="content" onClick={goToPostDetail}>
->>>>>>> jun
           <img src ={v.photo_src} />
           <figcaption>
             <h6>Tags</h6>
@@ -63,10 +32,6 @@ function Content(props){
 }
 
 function Main(props){
-<<<<<<< HEAD
-  console.log(data);
-=======
->>>>>>> jun
   // localStorage.setItem("userId", "cweald9")
   // const loggedUser = props.loggedUser;
   return(
--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,8 +1,4 @@
 import axios from "axios";
 export default axios.create({
-<<<<<<< HEAD
-    baseURL: "http://localhost/react/PURL/reactServer"
-=======
-    baseURL: "http://localhost/PURL/src/php"
->>>>>>> jun
+    baseURL: "http://localhost/PURL/react/src/php"
 })
